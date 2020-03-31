3bf491c07ad046a797048d0fa26782a41f9a48f2
roman <rpformic@mtu.edu>
2020-03-31 17:43:19 -0400

    Some small stylistic updates and the start of being able to add many ingredients at once. Not sure whats holding us up.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ApplicationRecord
+  attr_accessor :recipe_id, :ingredient_id
   belongs_to :recipe
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,5 @@
 class Recipe < ApplicationRecord
+  attr_accessor :name, :ingredients_attributes
   has_many :ingredients, :dependent => :delete_all
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank
   validates :title, presence: true, length: {minimum: 4}
