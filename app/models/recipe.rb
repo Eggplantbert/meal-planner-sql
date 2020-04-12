fa0216b9cb3351b36846f12ca67ed68606e72417
roman <rpformic@mtu.edu>
2020-04-12 16:02:58 -0400

    Add multiple ingredients at once

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   # attr_accessor :name, :ingredients_attributes
-  has_many :ingredients, :dependent => :delete_all
+  has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   validates :title, presence: true, length: {minimum: 4}
   validates :author, presence: true
