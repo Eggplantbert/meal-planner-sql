7c21475ce636d375ffb19b842f9f18f4108bd580
Ethan-Large <59936340+Ethan-Large@users.noreply.github.com>
2020-04-15 15:11:18 -0400

    Revert "RM-2019.3.3 <rocks@DESKTOP-SVD4QUV Merge branch 'master'"
    This reverts commit 8e0d8272876ec1c8541a181d323b5db2f0db9ad5, reversing
    changes made to 0db0c5fc7dba078d4eb00aaa2846d88662387b5d.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ApplicationRecord
   # attr_accessor :name, :ingredients_attributes
-  has_many :ingredients, dependent: :destroy
+  has_many :ingredients, :dependent => :delete_all
   accepts_nested_attributes_for :ingredients, reject_if: :all_blank, allow_destroy: true
   validates :title, presence: true, length: {minimum: 4}
   validates :author, presence: true
