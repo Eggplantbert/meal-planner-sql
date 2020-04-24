fd04e09d1f1b21644f908949622a2616e3991a0c
Alex Eby <aceby@mtu.edu>
2020-04-24 03:07:02 -0400

    Unit testing added with decent coverage

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class RecipesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get recipes_url
+    assert_response :success
+  end
+
+  test "should create recipe" do
+    assert_difference -> { Recipe.count } do
+      post recipes_path, params: { recipe: { title: "new recipe", author: "tester" } }, as: :json
+    end
+
+    assert_redirected_to edit_recipe_path(Recipe.last)
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "should destroy recipe" do
+    recipe = recipes(:apple_pie)
+    assert_difference('Recipe.count', -1) do
+      delete recipe_url(recipe)
+    end
+
+    assert_redirected_to recipes_path
+  end
+
+  test "should update recipe" do
+    recipe = recipes(:apple_pie)
+
+    patch recipe_url(recipe), params: { recipe: { author: "updated" } }
+
+    assert_redirected_to edit_recipe_path(recipe)
+    recipe.reload
+    assert_equal "updated", recipe.author
+  end
 end
--- test/system/add_ingredients_to_existings_test.rb
+++ test/system/add_ingredients_to_existings_test.rb
@@ -4,16 +4,10 @@ class AddIngredientsToExistingTest < ApplicationSystemTestCase
   test "add ingredient to existing recipe" do
     visit recipes_url
 
-    click_on "New Recipe"
-
-    fill_in "Title", with: "Apple Pie"
-    fill_in "Author", with: "test"
-    
-    click_on "Create Recipe"
     click_on "Edit"
     
-    fill_in "Name", with: "Apple"
-    fill_in "Amount", with: 3
+    fill_in "Name", with: "Honeycrisp"
+    fill_in "Amount", with: 5
     fill_in "Uom", with: "Apples"
     
     click_on "Update Recipe"
@@ -21,6 +15,6 @@ class AddIngredientsToExistingTest < ApplicationSystemTestCase
     take_screenshot
 
     assert_text "Apple Pie"
-    assert_text "Apple"
+    assert_text "Honeycrisp"
   end
 end
